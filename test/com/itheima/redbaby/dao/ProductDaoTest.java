cd9fb7b608a6477ccc3371c09a12a3b8e4fad8e0
Georgekaren <ad181600@qq.com>
2015-12-20 23:27:38 +0800

    base-重构

--- test/com/itheima/redbaby/dao/ProductDaoTest.java
+++ test/com/itheima/redbaby/dao/ProductDaoTest.java
@@ -1,8 +1,8 @@
-package com.itheima.redbaby.dao;
+package com.lianmeng.core.activity.dao;
 
 
  
-import com.itheima.redbaby.vo.ProdcutHistory;
+import com.lianmeng.core.activity.vo.ProdcutHistory;
 
 import android.test.AndroidTestCase;
 
@@ -13,7 +13,7 @@ public class ProductDaoTest extends AndroidTestCase {
 		productDao.delete(1);
 	}
 
- 	public void testAdd() {
+ 	public void testAdd() {    
 		ProductDao productDao = new ProductDao(getContext());
 		productDao.add(new ProdcutHistory(2, "奶粉", "http://192.168.1.139:8080/redbaby/images/product_04.jpg", 500.00, 400.00, 11, 360000));
  	}
